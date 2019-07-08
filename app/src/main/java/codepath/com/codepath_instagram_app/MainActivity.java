1fafeb638ca5d77bdc82e28caade59b4c892d0b8
Ruth-Ann Hazel Armstrong <ruthanna@fb.com>
2019-07-08 16:04:43 -0700

    gitignore added

--- app/src/main/java/codepath/com/codepath_instagram_app/MainActivity.java
+++ app/src/main/java/codepath/com/codepath_instagram_app/MainActivity.java
@@ -1,13 +1,24 @@
 package codepath.com.codepath_instagram_app;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText usernameInput;
+    private EditText passwordInput;
+    private Button loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        usernameInput = findViewById(R.id.etUserName);
+        passwordInput = findViewById(R.id.etPassword);
+
     }
 }
