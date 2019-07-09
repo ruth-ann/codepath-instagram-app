542798a4343d6e0c0150161c9192b3d1f4009aef
Ruth-Ann Hazel Armstrong <ruthanna@fb.com>
2019-07-08 21:25:11 -0700

    logout implemented, action bar bug fixed

--- app/src/main/java/codepath/com/codepath_instagram_app/HomeActivity.java
+++ app/src/main/java/codepath/com/codepath_instagram_app/HomeActivity.java
@@ -1,5 +1,6 @@
 package codepath.com.codepath_instagram_app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -7,18 +8,37 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.View;
+import android.widget.Button;
+
+import com.parse.ParseUser;
 
 public class HomeActivity extends AppCompatActivity {
 
 
+    private Button logoutBtn;
+    //private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        //Finds the logout button and sets a listener
+        logoutBtn = findViewById(R.id.btLogout);
+        logoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ParseUser.logOut();
+                //TODO set a logout message here
+                openLogin();
+
+            }
+
+        });
+
         // Find the toolbar view inside the activity layout
         Toolbar toolbar = findViewById(R.id.toolbar);
+
         // Sets the Toolbar to act as the ActionBar for this Activity window.
         // Make sure the toolbar exists in the activity and is not null
         setSupportActionBar(toolbar);
@@ -46,5 +66,10 @@ public class HomeActivity extends AppCompatActivity {
     }
 
 
+    private void openLogin(){
+        //takes the user to the login page
+        final Intent intent = new Intent(HomeActivity.this, MainActivity.class);
+        startActivity(intent);
+    }
 
 }
