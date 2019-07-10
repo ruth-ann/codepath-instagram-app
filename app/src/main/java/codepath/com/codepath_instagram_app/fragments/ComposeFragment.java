747174cd463c181f8f1f0686bff93e7a72de5f50
Ruth-Ann Hazel Armstrong <ruthanna@fb.com>
2019-07-09 23:54:11 -0700

    compose fragment implemented

--- app/src/main/java/codepath/com/codepath_instagram_app/fragments/ComposeFragment.java
+++ app/src/main/java/codepath/com/codepath_instagram_app/fragments/ComposeFragment.java
@@ -1,45 +1,78 @@
-package codepath.com.codepath_instagram_app;
+package codepath.com.codepath_instagram_app.fragments;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
-import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.app.Fragment;
+import android.support.v4.content.FileProvider;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
-public class HomeActivity extends AppCompatActivity {
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
+import java.io.File;
 
-  //  private static final String imagePath = "/Users/ruthanna/Desktop/facebookpost.jpg";
-    /*private EditText descriptionInput;
+import codepath.com.codepath_instagram_app.LoginActivity;
+import codepath.com.codepath_instagram_app.R;
+import codepath.com.codepath_instagram_app.TimelineActivity;
+import codepath.com.codepath_instagram_app.model.Post;
+
+import static android.app.Activity.RESULT_OK;
+
+public class ComposeFragment extends Fragment {
+
+    public final String TAG = "ComposeFragment";
+
+    //variables that enable camera use
+    public final static int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 1034;
+    public String photoFileName = "photo.jpg";
+    private File photoFile;
+
+    private EditText descriptionInput;
     private Button captureImageBtn;
     private ImageView postImageIv;
     private Button refreshBtn;
     private Button createBtn;
-    private Button logoutBtn;*/
+    private Button logoutBtn;
 
-    private BottomNavigationView bottomNavigationView;
+    //The onCreateView method is called when Fragment should create its View object
+    //hierarchy, either dynamically or via XML layout inflation
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
+        //Third parameter is false since the bottom navigation view takes care of attachment
+        return inflater.inflate(R.layout.fragment_compose, parent, false);
 
-   // private Toolbar toolbar;
+    }
 
-    //variables that enable camera use
-   // public final String TAG = "MyCustomApp";
-   /* public final static int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 1034;
-    public String photoFileName = "photo.jpg";
-    private File photoFile;*/
 
+    // This event is triggered soon after onCreateView().
+    // Any view setup should occur here.  E.g., view lookups and attaching view listeners.
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
-/*
-        descriptionInput = findViewById(R.id.etDescription);
+    public void onViewCreated(View view, Bundle savedInstanceState) {
 
-        postImageIv = findViewById(R.id.ivPostImage);
+        //Finds the views by id
+        descriptionInput = view.findViewById(R.id.etDescription);
+        postImageIv = view.findViewById(R.id.ivPostImage);
+        captureImageBtn = view.findViewById(R.id.btTakePhoto);
+        refreshBtn = view.findViewById(R.id.btRefresh);
+        createBtn = view.findViewById(R.id.btCreate);
 
         //Runs the function to launch the camera after the capture button is pressed
-        captureImageBtn = findViewById(R.id.btTakePhoto);
+
         captureImageBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -49,38 +82,41 @@ public class HomeActivity extends AppCompatActivity {
 
 
         //Finds the create post button and sets a listener
-        refreshBtn = findViewById(R.id.btRefresh);
+
         refreshBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent timelineIntent =  new Intent(HomeActivity.this, TimelineActivity.class);
+                Intent timelineIntent =  new Intent(getContext(), TimelineActivity.class);
                 startActivity(timelineIntent);
-               // loadTopPosts();
+                // loadTopPosts();
             }
 
         });
 
         //Finds the create post button and sets a listener
-        createBtn = findViewById(R.id.btCreate);
+
         createBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 final String description = descriptionInput.getText().toString();
                 final ParseUser user = ParseUser.getCurrentUser();
 
+                //    final File file = new File(imagePath);
+                //  final  ParseFile parseFile = new ParseFile(file);
+
                 //Ensures that a photo file is present
                 if (photoFile == null || postImageIv.getDrawable() == null){
                     Log.e(TAG, "No photo to submit");
-                    Toast.makeText(HomeActivity.this, "There is no photo!", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getContext(), "There is no photo!", Toast.LENGTH_SHORT).show();
                     return;
                 }
-                createPost(description, user, photoFile);
+                createPost(description, /*parseFile, */user, photoFile);
             }
 
         });
 
         //Finds the logout button and sets a listener
-        logoutBtn = findViewById(R.id.btLogout);
+        logoutBtn = view.findViewById(R.id.btLogout);
         logoutBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -90,32 +126,12 @@ public class HomeActivity extends AppCompatActivity {
 
             }
 
-        });  */
-
-        bottomNavigationView = findViewById(R.id.bottom_navigation);
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-             @Override
-             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                 switch (menuItem.getItemId()) {
-                     case R.id.action_home:
-                         Toast.makeText(HomeActivity.this, "Home!", Toast.LENGTH_LONG).show();
-                         return true;
-                     case R.id.action_compose:
-                         Toast.makeText(HomeActivity.this, "Compose!", Toast.LENGTH_LONG).show();
-                         return true;
-                     case R.id.action_profile:
-                         Toast.makeText(HomeActivity.this, "Profile!", Toast.LENGTH_LONG).show();
-                         return true;
-                     default: return true;
-                 }
-             }
-
-         });
-
-
-
+        });
     }
-/*
+
+
+
+
     private void launchCamera() {
         // create Intent to take a picture and return control to the calling application
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
@@ -126,12 +142,12 @@ public class HomeActivity extends AppCompatActivity {
         // required for API >= 24
         // See https://guides.codepath.com/android/Sharing-Content-with-Intents#sharing-files-with-api-24-or-higher
         //authority must match the authority in the provider tag of the xml file
-        Uri fileProvider = FileProvider.getUriForFile(HomeActivity.this, "com.codepath.fileprovider", photoFile);
+        Uri fileProvider = FileProvider.getUriForFile(getContext(), "com.codepath.fileprovider", photoFile);
         intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
 
         // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
         // So as long as the result is not null, it's safe to use the intent.
-        if (intent.resolveActivity(getPackageManager()) != null) {
+        if (intent.resolveActivity(getContext().getPackageManager()) != null) {
             // Start the image capture intent to take photo
             startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
         }
@@ -140,7 +156,7 @@ public class HomeActivity extends AppCompatActivity {
 
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode,  Intent data) {
+    public void onActivityResult(int requestCode, int resultCode,  Intent data) {
         if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
             if (resultCode == RESULT_OK) {
                 // by this point we have the camera photo on disk
@@ -149,7 +165,7 @@ public class HomeActivity extends AppCompatActivity {
                 // Load the taken image into a preview
                 postImageIv.setImageBitmap(takenImage);
             } else { // Result was a failure
-                Toast.makeText(this, "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getContext(), "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
             }
         }
     }
@@ -159,7 +175,7 @@ public class HomeActivity extends AppCompatActivity {
         // Get safe storage directory for photos
         // Use `getExternalFilesDir` on Context to access package-specific directories.
         // This way, we don't need to request external read/write runtime permissions.
-        File mediaStorageDir = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), TAG);
+        File mediaStorageDir = new File(getContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES), TAG);
 
         // Create the storage directory if it does not exist
         if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
@@ -173,7 +189,7 @@ public class HomeActivity extends AppCompatActivity {
     }
 
 
-    private void createPost(String description, ParseUser user, File photoFile){
+    private void createPost(String description, /*ParseFile imagefile, */ParseUser user, File photoFile){
         final Post newPost = new Post();
         newPost.setDescription(description);
         newPost.setImage(new ParseFile(photoFile));
@@ -184,8 +200,8 @@ public class HomeActivity extends AppCompatActivity {
             public void done(ParseException e) {
                 if (e == null){
                     Log.d("HomeActivity", "Create post success!");
-                     descriptionInput.setText("");
-                     postImageIv.setImageResource(0);
+                    descriptionInput.setText("");
+                    postImageIv.setImageResource(0);
                     // openLogin();
                 }else{
                     Log.e("HomeActivity", "Create post failure");
@@ -197,43 +213,17 @@ public class HomeActivity extends AppCompatActivity {
 
 
     // Menu icons are inflated just as they were with actionbar //TODO add stuff to toolbar
-    @Override
+  /*  @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_ig_timeline, menu);
         return true;
     }
-
+*/
 
     private void openLogin(){
         //takes the user to the login page
-        final Intent intent = new Intent(HomeActivity.this, LoginActivity.class);
+        final Intent intent = new Intent(getContext(), LoginActivity.class);
         startActivity(intent);
     }
-
-    /*private void loadTopPosts(){
-        final Post.Query postsQuery = new Post.Query();
-        //uses methods defined in Post model to get the top 20 posts
-        postsQuery.getTop().withUser();
-
-
-        //gets the posts stored in the database on a background thread
-        postsQuery.findInBackground(new FindCallback<Post>() {
-            @Override
-            public void done(List<Post> objects, ParseException e) {
-                if (e == null){
-                    for (int i = 0; i < objects.size(); i++) {
-                        Log.d("HomeActivity", "Post[" + i + "]="
-                                + objects.get(i).getDescription()
-                                + "\nusername = " + objects.get(i).getUser().getUsername()
-                        );
-
-                    }
-                }else{
-                    e.printStackTrace();
-                }
-            }
-        });
-    }*/
-
 }
