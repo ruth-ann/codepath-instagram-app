e7dc6db4fef10597813b1b5002510609a8a52f9e
Ruth-Ann Hazel Armstrong <ruthanna@fb.com>
2019-07-12 05:37:08 -0700

    unused timeline activity removed

--- app/src/main/java/codepath/com/codepath_instagram_app/PostAdapter.java
+++ app/src/main/java/codepath/com/codepath_instagram_app/PostAdapter.java
@@ -89,7 +89,6 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.ViewHolder> {
             likesTv = (TextView) itemView.findViewById(R.id.etLike);
             commentsTv = (TextView) itemView.findViewById(R.id.etComment);
             likeBtn = (ImageButton) itemView.findViewById(R.id.btLike);
-
             commentBtn = (ImageButton) itemView.findViewById(R.id.btComment);
 
 
@@ -193,45 +192,4 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.ViewHolder> {
     }
 
 
-    //stack overflow code
-  /*  public static class CircleTransform extends BitmapTransformation {
-        public CircleTransform(Context context) {
-            super(context);
-        }
-
-        @Override
-        protected Bitmap transform(BitmapPool pool, Bitmap toTransform, int outWidth, int outHeight) {
-            return circleCrop(pool, toTransform);
-        }
-
-        private static Bitmap circleCrop(BitmapPool pool, Bitmap source) {
-            if (source == null) return null;
-
-            int size = Math.min(source.getWidth(), source.getHeight());
-            int x = (source.getWidth() - size) / 2;
-            int y = (source.getHeight() - size) / 2;
-
-
-            Bitmap squared = Bitmap.createBitmap(source, x, y, size, size);
-
-            Bitmap result = pool.get(size, size, Bitmap.Config.ARGB_8888);
-            if (result == null) {
-                result = Bitmap.createBitmap(size, size, Bitmap.Config.ARGB_8888);
-            }
-
-            Canvas canvas = new Canvas(result);
-            Paint paint = new Paint();
-            paint.setShader(new BitmapShader(squared, BitmapShader.TileMode.CLAMP, BitmapShader.TileMode.CLAMP));
-            paint.setAntiAlias(true);
-            float r = size / 2f;
-            canvas.drawCircle(r, r, r, paint);
-            return result;
-        }
-
-        @Override
-        public String getId() {
-            return getClass().getName();
-        }
-
-    }*/
 }
